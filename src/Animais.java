11465ec0d45d44016ca595d887bfc9f2dd40c959
obiaggi <rafaelrocha.biagi@outlook.com>
2022-01-26 16:20:30 -0300

    Criando Projeto

---
+++ src/Animais.java
@@ -0,0 +1,35 @@
+public class Animais {
+    public static void main(String[] args) {
+
+        
+    }
+}
+
+
+//    crie um programa onde onde o usuario vai ter 2 opções 1 para vê as curiosidades e 2 para vê avaliação
+//        Caso escolha 1 . O usuario pode escolher entre 4 animais sendo :
+//
+//        1 - "Sapo-de-vidro"
+//        2 - "Porcos-do-mar"
+//        3 - "Peixe-lua"
+//        4 - "Cervo Vampiro"
+//
+//        Assim que a pessoa escolher entre os animais
+//        Mostre a seguinte mensagem
+//        {Se liga na curiosidade sobre o {aqui tem que conter o nome do animal}}[essa mensagem tem que ser utilizada independente do animal escolhido]
+//        {texto contendo uma curiosidade sobre o animal}
+//
+//        Depois fazer uma pesquisa perguntando qual nota a pessoa da para aquele animal
+//        Possiveis notas 0 1 2 3 4 5
+//        sendo 0 que não gostou nada do animal e 5 que achou incrivel
+//        depois disso mostrar automaticamente as avaliações.
+//
+//
+//        Caso escolha 2. O usuário terá acesso a média de avaliação de cada animal
+//        exemplo
+//        Sapo-de-vidro avaliação 3
+//        Porcos-do-mar 4
+//        e asism por diante ...
+//        Obs: eu dei valores aleatorios, esse resultado não representa minha preferencia
+//
+//        esse programa deve rodar no mínimo 8 vezes
